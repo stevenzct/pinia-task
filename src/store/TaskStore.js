130df88338f9df3cf9bf399e387c6329215a39af
stevenzct <stevencabugos138@gmail.com>
2025-09-16 12:01:41 +0800

    feat: add favorite and delete task details

--- src/store/TaskStore.js
+++ src/store/TaskStore.js
@@ -24,6 +24,15 @@ export const useTaskStore = defineStore('taskStore', {
     actions: {
         addTask(task) {
             this.tasks.push(task)
+        },
+        deleteTask(id) {
+            this.tasks = this.tasks.filter(t => {
+                return t.id !== id
+            })
+        },
+        toggleFav(id) {
+            const task = this.tasks.find(t => t.id === id)
+            task.isFav = !task.isFav
         }
     }
 })
